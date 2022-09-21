22704840fec0c323348b9be76510fe027a2e88a5
Sivak Hico <strukggg@gmail.com>
2022-09-21 15:39:58 +0200

    first movies list

--- src/parts/sivak.jsx
+++ src/parts/sivak.jsx
@@ -1,8 +1,10 @@
 function SivakToWatchList() {
+  const movies = ['Film1', 'Film2', 'Film3'];
+  const movieList = movies.map((item) => <li>{item}</li>)
   return (
     <div className="to_watch">
       <h3> I'm Sivak's To-Watch List </h3>
-      
+      <ul>{movieList}</ul>
     </div>
   );
 }
