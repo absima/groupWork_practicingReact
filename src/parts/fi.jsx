d56b4e3e51132a78d20ad27c2088de0c62b90d1e
Fi <vancetti@googlemail.com>
2022-09-22 10:30:21 +0200

    add calender layout

--- src/parts/fi.jsx
+++ src/parts/fi.jsx
@@ -1,12 +1,122 @@
 import React from 'react'
 
-
-
 function FiCalendar() {
   return (
-    <div className="calendar-container">
-      <h3> I'm Fi's Calendar. </h3>
+    <div className="container calender">
       <h2>Calender</h2>
+      <div className="container days">
+        <div class="day">
+
+        </div>
+      </div>
+      <div className="container boxes">
+        <div class="box">
+          <input type="text" id="whadda"></input>
+          <span class="number">1</span>
+        </div>
+        <div class="box">
+          <span class="number">2</span>
+        </div>
+        <div class="box">
+          <span class="number">3</span>
+        </div>
+        <div class="box">
+          <span class="number">4</span>
+        </div>
+        <div class="box">
+          <span class="number">5</span>
+        </div>
+        <div class="box">
+          <span class="number">6</span>
+        </div>
+        <div class="box">
+          <span class="number">7</span>
+        </div>
+        <div class="box">
+          <span class="number">8</span>
+        </div>
+        <div class="box">
+          <span class="number">9</span>
+        </div>
+        <div class="box">
+          <span class="number">10</span>
+        </div>
+        <div class="box">
+          <span class="number">11</span>
+        </div>
+        <div class="box">
+          <span class="number">12</span>
+        </div>
+        <div class="box">
+          <span class="number">13</span>
+        </div>
+        <div class="box">
+          <span class="number">14</span>
+        </div>
+        <div class="box">
+          <span class="number">15</span>
+        </div>
+        <div class="box">
+          <span class="number">16</span>
+        </div>
+        <div class="box">
+          <span class="number">17</span>
+        </div>
+        <div class="box">
+          <span class="number">18</span>
+        </div>
+        <div class="box">
+          <span class="number">19</span>
+        </div>
+        <div class="box">
+          <span class="number">20</span>
+        </div>
+        <div class="box">
+          <span class="number">21</span>
+        </div>
+        <div class="box">
+          <span class="number">22</span>
+        </div>
+        <div class="box">
+          <span class="number">23</span>
+        </div>
+        <div class="box">
+          <span class="number">24</span>
+        </div>
+        <div class="box">
+          <span class="number">25</span>
+        </div>
+        <div class="box">
+          <span class="number">26</span>
+        </div>
+        <div class="box">
+          <span class="number">27</span>
+        </div>
+        <div class="box">
+          <span class="number">28</span>
+        </div>
+        <div class="box">
+          <span class="number">29</span>
+        </div>
+        <div class="box">
+          <span class="number">30</span>
+        </div>
+        <div class="box">
+          <span class="number">31</span>
+        </div>
+        <div class="box">
+          <span class="number">32</span>
+        </div>
+        <div class="box">
+          <span class="number">33</span>
+        </div>
+        <div class="box">
+          <span class="number">34</span>
+        </div>
+        <div class="box">
+          <span class="number">35</span>
+        </div>
+      </div>
     </div>
   );
 }
