6226a8134137ee970ab3a38bc7306e82ef8d1e73
Simachew Mengiste <absima@gmail.com>
2022-09-22 12:34:46 +0300

    some movie covers added

--- src/parts/sima.jsx
+++ src/parts/sima.jsx
@@ -8,39 +8,74 @@
 
 function SimaMovieList() {
   return (
-    <div className="container movie_list">
-      <h3> I'm Sima's Movie List. </h3>
-      <div className="t_movies">
-        <div className="movie">
+    // <div className="container1 movie_list">
+    <> 
+      <div className="container simapart">
+        <div className="row bgrd">
+          <div className="title_div">
+            <span className="reco">Today's Recommendation</span>
+            <h1 className="title_perfumier">The Perfumier</h1>
+            <button type="button" className="btn btn-warning button_perfumier"><i className="bi bi-play"> Watch Now</i> </button>
+          </div>
         </div>
 
-        <div className="movie">
-        </div>
-        
-        <div className="movie">
-        </div>
-        
-        <div className="movie">
-        </div>
-        
-      </div>
-      <div className="t_seiries">
-      <div className="movie">
+        <div className="row">
+          <div className="col-12">
+            <h4 className="trending">Trending movies </h4>
+          </div>
         </div>
 
-        <div className="movie">
+        <div className="row row-cols-2 row-cols-sm-3 row-cols-md-4 row-cols-lg-5 movie_row">
+          <div className="col">
+            <img src="https://raw.githubusercontent.com/brixiobodino/coffeholic/main/image1.PNG" className="zoom"/>
+          </div>
+          <div className="col">
+            <img src="https://raw.githubusercontent.com/brixiobodino/coffeholic/main/image1.PNG" className="zoom" />
+          </div>
+          <div className="col">
+            <img src="https://raw.githubusercontent.com/brixiobodino/coffeholic/main/image1.PNG" className="zoom" />
+          </div>
+          <div className="col">
+            <img src="https://raw.githubusercontent.com/brixiobodino/coffeholic/main/image1.PNG" className="zoom" />
+          </div>
+          <div className="col">
+            <img src="https://raw.githubusercontent.com/brixiobodino/coffeholic/main/image1.PNG" className="zoom" />
+          </div>
+          {/* <div class="col">
+      <img src="https://raw.githubusercontent.com/brixiobodino/coffeholic/main/image1.PNG">
+          </div> */}
         </div>
-        
-        <div className="movie">
+
+        <div className="row">
+          <div className="col-12">
+            <h4 className="trending">Trending Tv Series </h4>
+          </div>
         </div>
-        
-        <div className="movie">
+
+        <div className="row row-cols-2 row-cols-sm-3 row-cols-md-4 row-cols-lg-5">
+          <div className="col">
+            <img src="https://raw.githubusercontent.com/brixiobodino/coffeholic/main/image1.PNG" className="zoom" />
+          </div>
+          <div className="col">
+            <img src="https://raw.githubusercontent.com/brixiobodino/coffeholic/main/image1.PNG" className="zoom" />
+          </div>
+          <div className="col">
+            <img src="https://raw.githubusercontent.com/brixiobodino/coffeholic/main/image1.PNG" className="zoom" />
+          </div>
+          <div className="col">
+            <img src="https://raw.githubusercontent.com/brixiobodino/coffeholic/main/image1.PNG" className="zoom" />
+          </div>
+          <div className="col">
+            <img src="https://raw.githubusercontent.com/brixiobodino/coffeholic/main/image1.PNG" className="zoom" />
+          </div>
+          {/* <div class="col">
+      <img src="https://raw.githubusercontent.com/brixiobodino/coffeholic/main/image1.PNG">
+          </div> */}
         </div>
       </div>
       
-    </div>
+    </>
   );
 }
 
 export default SimaMovieList;
-
