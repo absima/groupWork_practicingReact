9206941d7405d261316449caee7a263292b4ed18
Christopher Shelley <dont@forgetmy.email>
2022-09-22 19:45:32 +0200

    add nonsense

--- src/myApp.jsx
+++ src/myApp.jsx
@@ -4,6 +4,7 @@ import FiCalendar from './parts/fi.jsx'
 import SivakToWatchList from './parts/sivak.jsx'
 import SimaMovieList from './parts/sima.jsx'
 import Container from 'react-bootstrap/Container';
+import Table from './parts/Table.jsx'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
@@ -14,6 +15,7 @@ export default function MyApp() {
     <Container>
       <h1>Welcome to MyFlix</h1>
       <ArthiHeader />
+      <Table />
       <FiCalendar />
       <SivakToWatchList />
       <SimaMovieList />
