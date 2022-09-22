c978197d381cf00ea7ba3dc59d940f73cf6e1e7f
Simachew Mengiste <absima@gmail.com>
2022-09-22 15:05:12 +0300

    correcting off margin

--- src/parts/sima.jsx
+++ src/parts/sima.jsx
@@ -10,7 +10,6 @@ function SimaMovieList() {
   return (
     // <div className="container1 movie_list">
     <> 
-<<<<<<< Updated upstream
       <div className="container-fluid simapart">
         <div className="row bgrd">
           <div className="title_div">
@@ -18,56 +17,6 @@ function SimaMovieList() {
             <h1 className="title_perfumier">The Perfumier</h1>
             <button type="button" className="btn btn-warning button_perfumier"><i className="bi bi-play"> Watch Now</i> </button>
           </div>
-=======
-      <div className="top">
-          <div className="columns">
-            <div className="column is-full featured_wrapper p-0">
-              <img src="https://raw.githubusercontent.com/brixiobodino/coffeholic/main/banner.jpg" className="featured" />
-              <div className="title_wrapper">
-                <span className="has-text-white">Trending Today</span>
-                <h1 className="title is-1 has-text-white">The Untold Story of the great Lorem Ipsum</h1>
-                <button className="button is-medium">Watch It Now</button>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="container">
-          <div className="columns is-multiline p-0 pt-6 last">
-            <div className="column is-full">
-              <p className="has-text-white">Action Movies Collection</p>
-            </div>
-            <div className="column is-one-quarter">
-              <img src="https://raw.githubusercontent.com/brixiobodino/coffeholic/main/image1.PNG" />
-            </div>
-            <div className="column is-one-quarter">
-              <img src="https://raw.githubusercontent.com/brixiobodino/coffeholic/main/image2.PNG" />
-            </div>
-            <div className="column is-one-quarter">
-              <img src="https://raw.githubusercontent.com/brixiobodino/coffeholic/main/img3.PNG" />
-            </div>
-            <div className="column is-one-quarter">
-              <img src="https://raw.githubusercontent.com/brixiobodino/coffeholic/main/img4.PNG" />
-            </div>
-          </div>
-          <div className="columns last">
-            <div className="column is-one-quarter">
-              <img src="https://raw.githubusercontent.com/brixiobodino/coffeholic/main/img5.PNG" />
-            </div>
-            <div className="column is-one-quarter">
-              <img src="https://raw.githubusercontent.com/brixiobodino/coffeholic/main/img6.PNG" />
-            </div>
-            <div className="column is-one-quarter">
-              <img src="https://raw.githubusercontent.com/brixiobodino/coffeholic/main/img7.PNG" />
-            </div>
-            <div className="column is-one-quarter">
-              <img src="https://raw.githubusercontent.com/brixiobodino/coffeholic/main/img8.PNG" />
-            </div>
-          </div>
-        </div>
-      {/* <h3> I'm Sima's Movie List. </h3>
-      <div className="t_movies">
-        <div className="movie">
->>>>>>> Stashed changes
         </div>
 
         <div className="row">
@@ -124,7 +73,7 @@ function SimaMovieList() {
             <img src="https://raw.githubusercontent.com/brixiobodino/coffeholic/main/image1.PNG" className="zoom" />
           </div>
         </div>
-      </div> */}
+      </div>
       
     </>
   );
