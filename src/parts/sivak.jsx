6a03b7d6a7a07fb7c1d97ae3a75a10bab995345a
Sivak Hico <strukggg@gmail.com>
2022-09-23 11:33:04 +0200

    no error

--- src/parts/sivak.jsx
+++ src/parts/sivak.jsx
@@ -1,12 +1,25 @@
 import './sivak.css'
+import { useState } from 'react'
 
 function SivakToWatchList() {
-  const movies = [{ title: 'Film 1', img: 'url' }, { title: 'Film 2' }, { title: 'Film 3' }];
-  const movieList = movies.map((item) => <li key={item.title}>{item.title}</li>)
+
+  const [movies, setMovies] = useState([
+    { title: 'Film 1', img: 'src/images/watch-list/02.jpg' },
+    { title: 'Film 2', img: 'src/images/watch-list/03.jpg' },
+    { title: 'Film 3', img: 'src/images/watch-list/04.jpg' }]);
+
+  const displayMovies = (movie) => <li key={movie.title}><a href="">{movie.title}</a></li>
+  const movieList = movies.map(displayMovies)
+
+  const displayImages = (movie) => <img src={movie.img} alt={movie.title} />
+  const imgList = movies.map(displayImages)
+
+  const classRoom = (student) => <li>{student.name}</li>
+
   return (
     <div className="to_watch">
       <h3>My Favorite Watch List</h3>
-      <ol>{movieList}</ol>
+      <ol>{imgList}{movieList}</ol>
     </div>
   );
 }
